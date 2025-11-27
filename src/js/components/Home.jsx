51419af0ff43cc9b8f1142c8ff93f981b0190e26
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2025-11-27 02:29:44 +0000

    :art: Refactor Home component and add color generation feature

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const getRandomColor = () => {
+	const hexValues = "0123456789ABCDEF";
+	let newColor = "#";
+	for (let i = 0; i < 6; i++) {
+		const randomIndex = Math.floor(Math.random() * hexValues.length);
+		newColor += hexValues[randomIndex];
+	}
+	return newColor;
+};
 
-//create your first component
+const ColorCard = ({ color }) => (
+	<div className="card m-3 rounded-0" style={{ width: "18rem", backgroundColor: color }}>
+		<div className="card-body">
+			<h5 className="card-title text-white">{color}</h5>
+		</div>
+	</div>
+);
+
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [currentColor, setCurrentColor] = useState(getRandomColor());
+	
+	const [colorList, setColorList] = useState(["#6932a8"]);
+
+	return (
+		<div className="text-center container mt-5 p-4" style={{ backgroundColor: currentColor }}>
+			<div className="mt-4">
+				<h1 className="display-4 text-primary">
+					Welcome to our color palette generator!
+				</h1>
+				<p className="lead text-secondary">
+					Generate beautiful color palettes with ease and inspiration.
+				</p>
+
+				<button className="btn btn-lg btn-success mt-3"
+					onClick={() => setColorList([ getRandomColor(), ...colorList ])}
+				>
+					Generate color!
+				</button>
+			</div>
+			<div className="container d-flex flex-wrap justify-content-center mt-5">
+				{
+					colorList.map((color, index) => (
+						<ColorCard key={index} color={color} />
+					))
+				}
+			</div>
 		</div>
 	);
 };
