cfc88e6458358d5661a1777b6a302826c5bd56e0
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2025-11-27 03:49:52 +0000

    :recycle: Refactor ColorCard component and add color logging on mount/unmount

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 
 const getRandomColor = () => {
 	const hexValues = "0123456789ABCDEF";
@@ -10,22 +10,41 @@ const getRandomColor = () => {
 	return newColor;
 };
 
-const ColorCard = ({ color }) => (
-	<div className="card m-3 rounded-0" style={{ width: "18rem", backgroundColor: color }}>
+const ColorCard = ({ color }) => {
+
+	useEffect(() => {
+		// onLoad();
+		console.log(`ColorCard mounted with color: ${color}`);
+
+		// unMount();
+		return () => {
+			console.log(`ColorCard with color: ${color} is unmounting`);
+		};
+
+	}, []); // updates
+
+	return <div className="card m-3 rounded-0" style={{ width: "18rem", backgroundColor: color }}>
 		<div className="card-body">
 			<h5 className="card-title text-white">{color}</h5>
 		</div>
 	</div>
-);
+};
 
 const Home = () => {
 
 	const [currentColor, setCurrentColor] = useState(getRandomColor());
-	
-	const [colorList, setColorList] = useState(["#6932a8"]);
+	const [ colorList, setColorList ] = useState(["#32a852"])
+
+	const [ currentDate, setCurrentDate ] = useState(new Date().toLocaleString())
+
+	useEffect(() => { // unica vez por que es como un onload
+		setInterval(() => {
+			setCurrentDate(new Date().toLocaleString())
+		},1000)
+	},[])
 
 	return (
-		<div className="text-center container mt-5 p-4" style={{ backgroundColor: currentColor }}>
+		<div className="text-center container d-flex flex-column mt-5 p-4" style={{ backgroundColor: currentColor }}>
 			<div className="mt-4">
 				<h1 className="display-4 text-primary">
 					Welcome to our color palette generator!
@@ -33,18 +52,23 @@ const Home = () => {
 				<p className="lead text-secondary">
 					Generate beautiful color palettes with ease and inspiration.
 				</p>
-
-				<button className="btn btn-lg btn-success mt-3"
-					onClick={() => setColorList([ getRandomColor(), ...colorList ])}
-				>
-					Generate color!
+				<button className="btn btn-warning" onClick={() => setColorList([...colorList, getRandomColor()])}>
+					Add new Color!
 				</button>
+
 			</div>
-			<div className="container d-flex flex-wrap justify-content-center mt-5">
+			<div className="container d-flex flex-column justify-content-center align-items-center mt-5">
+				<p className="text-white">{currentDate}</p>
 				{
-					colorList.map((color, index) => (
-						<ColorCard key={index} color={color} />
-					))
+					colorList.map((color, index) => {
+						return <div key={index} 
+							onClick={
+								() => setColorList(colorList.filter(item => item != color))
+							}
+						>
+							<ColorCard color={color} />
+						</div>
+					})
 				}
 			</div>
 		</div>
